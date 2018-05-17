56d6ffc3041324c3b5ba3ccf479d88d89ebda692
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 12:58:49 +0800

    environment variable

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: '1501563220',
-  channelSecret: '225b6427eb00f5ab6a7db90a7244b893',
-  channelAccessToken: 'EXUXNZCjK33qI1O9Rmgq5R3CMnzIMfB1Z93LDyuOyeODKLSECo/PdEITLWMWMaP9SVYXkSTzjhdeKHMGFiZIq6/B/emRhYhrHxqMNSrewD7WsJJ6xIwLmeH2rFPoy2coPCOyEtrX4efmO5nDCiBs6wdB04t89/1O/w1cDnyilFU='
+  channelId: process.env.Line_Channel_ID,
+  channelSecret: process.env.Line_Channel_Secret,
+  channelAccessToken: process.env.Line_Channel_Access_Token
 });
 
 bot.on('message', function(event) {
