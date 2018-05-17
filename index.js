c5e3a109ee50a942240a09f1124f613be367ffca
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 16:18:39 +0800

    test reply

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ var bot = linebot({
 //   console.log(event); //把收到訊息的 event 印出來看看
 // });
 bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log('Success', data);
-  }).catch(function (error) {
-    console.log('Error', error);
-  });
+  console.log(event);
+  // event.reply(event.message.text).then(function (data) {
+  //   console.log('Success', data);
+  // }).catch(function (error) {
+  //   console.log('Error', error);
+  // });
 });
 
 const app = express();
 const linebotParser = bot.parser();
 app.post('/webhook', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+// Change port on Heroku
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
