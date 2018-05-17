476b1efee7d329cc026af849047d173e6f9dbd50
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 16:15:03 +0800

    test reply

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // for initial webhook verification
 var linebot = require('linebot');
-var express = require('express');
 
+var express = require('express');
 
 var bot = linebot({
   channelId: process.env.Line_Channel_ID,
@@ -12,8 +12,6 @@ var bot = linebot({
 // bot.on('message', function(event) {
 //   console.log(event); //把收到訊息的 event 印出來看看
 // });
-
-
 bot.on('message', function (event) {
   event.reply(event.message.text).then(function (data) {
     console.log('Success', data);
@@ -26,15 +24,8 @@ const app = express();
 const linebotParser = bot.parser();
 app.post('/webhook', linebotParser);
 
-bot.listen('/test', process.env.PORT || 8080, function () {
-  console.log('LineBot is running.');
-});
-
-
-
-
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-// var server = app.listen(process.env.PORT || 8080, function() {
-//   var port = server.address().port;
-//   console.log("App now running on port", port);
-// });
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
