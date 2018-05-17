1a395ccb01e24c83f890e03aec77a597d4b2086b
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 17:59:35 +0800

    TEST PUSH: delete then()

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ var bot = linebot({
 
 bot.on('message', function (event) {
   console.log('## Message From: ', event.source.userId);
-  bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0','Now you see me!').then(
-    switch (event.message.type) {
+  switch (event.message.type) {
         case 'text':
             console.log('## Message: ', event.message.text)
             switch (event.message.text) {
@@ -37,9 +36,10 @@ bot.on('message', function (event) {
                         previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
                     });
                     break;
-                // case 'Push':
-                    // bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0', 'Push to group');
-                    // break;
+                case 'Push':
+                    bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0', 'Push to group');
+                    bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0', 'Now you see me!');
+                    break;
                 default:
                     console.log('卍卍卍卍 不知道要幹麻 卍卍卍卍');
             }
@@ -75,7 +75,7 @@ bot.on('message', function (event) {
         default:
             event.reply('Unknow message: ' + JSON.stringify(event));
             break;
-    })
+    }
 });
 
 const app = express();
