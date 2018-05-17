fa85a76f38565526e90e9b5cd34e4867024eb1c6
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 16:35:37 +0800

    trying autoreply

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var linebot = require('linebot');
 var express = require('express');
+const bodyParser = require('body-parser');
 
 var bot = linebot({
   channelId: process.env.Line_Channel_ID,
@@ -13,13 +14,13 @@ var bot = linebot({
 
 bot.on('message', function (event) {
   console.log('## Message From: ', event.source.userId);
-  bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0','Now you see me!');
+  // bot.push('U6d38c978c5bdd2c68ec3d8dd6f1638a0','Now you see me!');
   // console.log('## Message TEXT: ', event.message.text);
-  // event.reply(event.message.text).then(function (data) {
-  //   console.log('Success', data);
-  // }).catch(function (error) {
-  //   console.log('Error', error);
-  // });
+  event.reply('Now you see me').then(function (data) {
+    console.log(' --> Success!!', data);
+  }).catch(function (error) {
+    console.log('FOOL --!! -- Error', error);
+  });
 });
 
 const app = express();
