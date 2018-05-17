6131f47367f50ae1cb425bf6f537b956ef7bd8e0
Anton Cheng <b03705027@ntu.edu.tw>
2018-05-17 13:12:40 +0800

    change webhook url to domain:webhook/

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on('message', function(event) {
 
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post('/webhook', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
